afb35d845fc765ce657cd69c640d9be7dc3f390a
Akant <akantmalviya786@gmail.com>
2022-11-17 20:47:30 +0530

    Final Commit

--- TCD.py
+++ TCD.py
@@ -3,13 +3,14 @@ import os
 import requests
 import tkinter
 from tkinter import ttk, messagebox
+import subprocess
 
 
 # Fonts
 copyryt = u"\u00A9"
 trademark = u"\u2122"
 font4 = ('Times', 20)
-font1 = ('Times', 20, 'bold')
+font1 = ('Times', 18, 'bold')
 font2 = ('Times', 15)
 font3 = ('Times', 15, 'bold')
 
@@ -17,7 +18,7 @@ font3 = ('Times', 15, 'bold')
 window = tkinter.Tk()
 window.title("TEST CASE DOWNLOADER")
 window.configure(background="#fff")
-window.geometry("1000x390")
+window.geometry("1050x390")
 window.option_add("*tearOff", False)
 window.resizable(False, False)
 photo = tkinter.PhotoImage(file='images/xml.png')
@@ -72,7 +73,8 @@ def show_about_info():
         4. Download Any Specific Client Test Case.\n
         5. Add Any Client And Its Envelope In The Software.\n
         6. Delete Any Client From The Software.\n
-        7. Exit\n\n\t\t\t\t\t{brand}
+        7. For Creating Multisource Test Case.\n
+        8. Exit\n\n\t\t\t\t\t{brand}
         ''')
 
 
@@ -397,6 +399,170 @@ def downloadFunction():
         change_color(0)
 
 
+def multisourceFunction():
+    global envelope_headpath, envelope_footpath, client_link
+    success_count = 0
+    if Clientstr.get() != "" and Clientstr.get() in clients and FileNameStr.get() != "":
+        if CheckVar3.get() == 1 or CheckVar4.get() == 1 or CheckVar5.get() == 1:
+            if len(FileNameStr.get())==11 and FileNameStr.get().isdigit():
+                status_var.set(f"<Please wait, Multisource XML is downloading...>")
+                change_color(3)
+                Employee_ID = FileNameStr.get().upper()
+                Client_name = Clientstr.get().upper()
+                # Fetching Ariel Payroll XML
+                if CheckVar3.get() == 1:
+                    try:
+                        scriptpath = os.path.join(os.getcwd(), "imp", "downloadlinks", f"{Client_name}_ArielPayroll.ps1")
+                        with open(scriptpath, "r") as f:
+                            commands = f.read()
+
+                        commands = commands.replace("00000000000", Employee_ID)
+                        result = subprocess.run(["powershell", "-command", commands], capture_output=True)
+                        payrolldata = result.stdout
+                        payrollxml = "<ArielPayroll>" + payrolldata.decode() + "</ArielPayroll>"
+                        payrollxmlpath = os.path.join(os.getcwd(),"xmls","multisource", f"{Employee_ID}PR.xml")
+                        if Employee_ID in payrollxml:
+                            with open(payrollxmlpath, "w") as f:
+                                f.write(payrollxml)
+                            success_count += 1
+                        else:
+                            messagebox.showerror(title="Abort", message="File not found at ArielPayroll portal")
+                            status_var.set(f"<Employee ID is not available at ArielPayroll Portal...>")
+                            change_color(2)
+
+                    except(IOError, UnboundLocalError, FileNotFoundError):
+                        status_var.set(f"<Please enter a valid client...>")
+                        change_color(1)
+
+                if CheckVar4.get() == 1 or CheckVar5.get() == 1:
+                    try:
+                        dbpath = os.path.join(os.getcwd(), "imp", "downloadlinks", f"{Client_name}_ArielDB.txt")
+                        with open(dbpath) as f:
+                            dbfilepath = f.read()
+
+                        i = 0;
+                        txtfiles = dict()
+                        while i < len(Employee_ID) - 1:
+                            i += 1
+                            dbfilepath = dbfilepath + "\\" + Employee_ID[0:i]
+
+                        for dbfilename in os.listdir(dbfilepath):
+                            if dbfilename.endswith("INF.txt"):
+                                with open(os.path.join(dbfilepath, dbfilename)) as f:
+                                    if Employee_ID in f.read():
+                                        txtfiles[dbfilename] = float(
+                                            os.path.getctime(os.path.join(dbfilepath, dbfilename)))
+
+                        dbfilename = max(txtfiles, key=txtfiles.get)
+
+                    except(IOError, UnboundLocalError, FileNotFoundError):
+                        status_var.set(f"<Please enter a valid client & employee id>")
+                        change_color(1)
+
+                if CheckVar4 == 1:
+                    # CRM TEST CASE ID Or Fetch Data for Ariel360 Case
+                    try:
+                        with open(os.path.join(dbfilepath, dbfilename)) as f:
+                            datalist = f.read().split(",")
+
+                        CaseID = [i for i in datalist if "RequestCode=" in i]
+                        CaseID = CaseID[0].replace("RequestCode=", "")
+                        CaseID = CaseID.replace('"', "")
+                        CaseID = CaseID.replace("'", "")
+                        CaseID = CaseID.replace(" ", "")
+                        client_link = os.path.join(os.getcwd(), 'imp', 'downloadlinks', f'{Client_name}_Ariel360.txt')
+                        with open(client_link, 'r') as f:
+                            client_link = f.read()
+                        testcase = client_link[client_link.find("=") + 1:client_link.find("&")]
+                        CRMfile = requests.get(client_link.replace(testcase, CaseID))
+                        CRMfile = "<Ariel360>" + CRMfile.content.decode() + "</Ariel360>"
+                        CRMfile = CRMfile.replace('<?xml version="1.0" encoding="utf-8"?>', "")
+                        CRMfolderpath = os.path.join(os.getcwd(), "xmls", "multisource", f"{CaseID}.xml")
+                        with open(CRMfolderpath, "w") as f:
+                            f.write(CRMfile)
+                            success_count += 1
+                        status_var.set(f"<{CaseID} has been downloaded for {Client_name}>")
+                        change_color(1)
+
+                    except(IOError, UnboundLocalError, FileNotFoundError):
+                        status_var.set(f"<Please enter a valid client & employee id>")
+                        change_color(0)
+
+                    except requests.exceptions.MissingSchema:
+                        status_var.set(f"Test case not found on the portal!")
+                        messagebox.showerror(title="Abort", message="File not found at CRM portal")
+                        change_color(2)
+
+                if CheckVar5.get() == 1:
+                    # fetching ArielDB XML
+                    try:
+                        dbfilename = dbfilename.replace("INF.txt", "STM.xml")
+                        with open(os.path.join(dbfilepath, dbfilename), "r") as f:
+                            ArielDBxml = f.read()
+                            ArielDBxml = ArielDBxml.replace('<?xml version="1.0" encoding="utf-8"?>', "")
+                            ArielDBxml = "<ArielDB>" + ArielDBxml + "</ArielDB>"
+
+                        with open(os.path.join(os.getcwd(), "xmls", "multisource", f"{Employee_ID}STM.xml"), "w") as f:
+                            f.write(ArielDBxml)
+                            success_count += 1
+
+                    except(IOError, UnboundLocalError, FileNotFoundError):
+                        status_var.set(f"<Please enter a valid client & employee id>")
+                        change_color(0)
+
+                if success_count == 3 and payrollxml != "" and CRMfile != "" and ArielDBxml != "":
+                    envelope_headpath = os.path.join(os.getcwd(), 'envelopes', f'{Client_name}_Multisource_Header.txt')
+                    envelope_footpath = os.path.join(os.getcwd(), 'envelopes', f'Multisource_Footer.txt')
+                    with open(envelope_headpath, "r") as f, open(envelope_footpath, "r") as f1:
+                        header = f.read()
+                        footer = f1.read()
+                    multisourceXML = header + payrollxml + CRMfile + ArielDBxml + footer
+                    with open(os.path.join(os.getcwd(), "xmls", "multisource", f"{Employee_ID}Multisource.xml"), "w") as f:
+                        f.write(multisourceXML)
+                    messagebox.showinfo(title="Task completed", message="Test case downloaded successfully")
+                    status_var.set("Multisource XML has been downloaded successfully")
+                    change_color(1)
+                else:
+                    messagebox.showwarning(title="Warning!", message="Something went wrong, please check location!")
+                    status_var.set(f"<Please provide valid Employee ID>")
+                    change_color(0)
+
+            else:
+                FileNameStr.set("")
+                Clientstr.set("")
+                CheckVar3.set(1)
+                CheckVar4.set(1)
+                CheckVar5.set(1)
+                status_var.set(f"<Please provide valid Employee ID>")
+                change_color(0)
+
+        else:
+            FileNameStr.set("")
+            Clientstr.set("")
+            CheckVar3.set(1)
+            CheckVar4.set(1)
+            CheckVar5.set(1)
+            status_var.set(f"<Please Select Any Libraries>")
+            change_color(0)
+    elif FileNameStr.get() == "":
+        FileNameStr.set("")
+        Clientstr.set("")
+        CheckVar3.set(1)
+        CheckVar4.set(1)
+        CheckVar5.set(1)
+        status_var.set(f"<Please provide Employee ID>")
+        change_color(0)
+
+    else:
+        FileNameStr.set("")
+        Clientstr.set("")
+        CheckVar3.set(1)
+        CheckVar4.set(1)
+        CheckVar5.set(1)
+        status_var.set(f"<Please Select Any Client>")
+        change_color(0)
+
+
 def New():
     FileNameStr.set("")
     Clientstr.set("")
@@ -408,12 +574,23 @@ def New():
     b3.grid_remove()
     b4.grid_remove()
     b5.grid_remove()
+    b6.grid_remove()
+    c3.grid_remove()
+    c4.grid_remove()
+    c5.grid_remove()
+    l4.grid_remove()
     if e2["state"] == "disabled":
         e2["state"] = "normal"
     if c1["state"] == "disabled":
         c1["state"] = "normal"
     if c2["state"] == "disabled":
         c2["state"] = "normal"
+    if e2.winfo_ismapped() == 0:
+        e2.grid(row=2, column=2, padx=10, pady=10, ipadx=2, ipady=2)
+    if c1.winfo_ismapped() == 0:
+        c1.grid(row=3, column=1, ipadx=2, ipady=2, rowspan=1, columnspan=2)
+    if c2.winfo_ismapped() == 0:
+        c2.grid(row=3, column=2, ipadx=2, ipady=2, rowspan=1, columnspan=2)
     if d1.winfo_ismapped() == 0:
         d1.grid(row=1, column=2, padx=10, pady=10, ipadx=2, ipady=2)
     if b1.winfo_ismapped() == 0:
@@ -434,9 +611,20 @@ def auto_mate():
     b3.grid_remove()
     b4.grid_remove()
     b5.grid_remove()
+    b6.grid_remove()
     l5.grid_remove()
+    c3.grid_remove()
+    c4.grid_remove()
+    c5.grid_remove()
+    l4.grid_remove()
     if e2["state"] == "normal" or e2["state"] == "active":
         e2["state"] = "disabled"
+    if e2.winfo_ismapped() == 0:
+        e2.grid(row=2, column=2, padx=10, pady=10, ipadx=2, ipady=2)
+    if c1.winfo_ismapped() == 0:
+        c1.grid(row=3, column=1, ipadx=2, ipady=2, rowspan=1, columnspan=2)
+    if c2.winfo_ismapped() == 0:
+        c2.grid(row=3, column=2, ipadx=2, ipady=2, rowspan=1, columnspan=2)
     if d1.winfo_ismapped() == 0:
         d1.grid(row=1, column=2, padx=10, pady=10, ipadx=2, ipady=2)
     if b2.winfo_ismapped()==0:
@@ -459,12 +647,23 @@ def addClient():
     b2.grid_remove()
     b4.grid_remove()
     b5.grid_remove()
+    b6.grid_remove()
+    c3.grid_remove()
+    c4.grid_remove()
+    c5.grid_remove()
+    l4.grid_remove()
     if e2["state"] == "disabled":
         e2["state"] = "normal"
     if c1["state"] == "disabled":
         c1["state"] = "normal"
     if c2["state"] == "disabled":
         c2["state"] = "normal"
+    if e2.winfo_ismapped() == 0:
+        e2.grid(row=2, column=2, padx=10, pady=10, ipadx=2, ipady=2)
+    if c1.winfo_ismapped() == 0:
+        c1.grid(row=3, column=1, ipadx=2, ipady=2, rowspan=1, columnspan=2)
+    if c2.winfo_ismapped() == 0:
+        c2.grid(row=3, column=2, ipadx=2, ipady=2, rowspan=1, columnspan=2)
     if e1.winfo_ismapped() == 0:
         e1.grid(row=1, column=2, padx=10, pady=10, ipadx=2, ipady=2)
     if l5.winfo_ismapped() == 0:
@@ -486,12 +685,23 @@ def delClient():
     b2.grid_remove()
     b3.grid_remove()
     b5.grid_remove()
+    b6.grid_remove()
+    c3.grid_remove()
+    c4.grid_remove()
+    c5.grid_remove()
+    l4.grid_remove()
+    if e2.winfo_ismapped() == 0:
+        e2.grid(row=2, column=2, padx=10, pady=10, ipadx=2, ipady=2)
     if e2["state"] == "normal" or e2["state"] == "active":
         e2["state"] = "disabled"
     if c1["state"] == "normal" or c1["state"] == "active":
         c1["state"] = "disabled"
     if c2["state"] == "normal" or c2["state"] == "active":
         c2["state"] = "disabled"
+    if c1.winfo_ismapped() == 0:
+        c1.grid(row=3, column=1, ipadx=2, ipady=2, rowspan=1, columnspan=2)
+    if c2.winfo_ismapped() == 0:
+        c2.grid(row=3, column=2, ipadx=2, ipady=2, rowspan=1, columnspan=2)
     if d1.winfo_ismapped() == 0:
         d1.grid(row=1, column=2, padx=10, pady=10, ipadx=2, ipady=2)
     if l3.winfo_ismapped() == 0:
@@ -513,12 +723,25 @@ def download():
     b3.grid_remove()
     b4.grid_remove()
     l5.grid_remove()
+    b6.grid_remove()
+    c3.grid_remove()
+    c4.grid_remove()
+    c5.grid_remove()
+    l4.grid_remove()
     if e2["state"] == "disabled":
         e2["state"] = "normal"
     if c1["state"] == "disabled":
         c1["state"] = "normal"
     if c2["state"] == "disabled":
         c2["state"] = "normal"
+    if e2.winfo_ismapped() == 0:
+        e2.grid(row=2, column=2, padx=10, pady=10, ipadx=2, ipady=2)
+    if d1.winfo_ismapped() == 0:
+        d1.grid(row=1, column=2, padx=10, pady=10, ipadx=2, ipady=2)
+    if c1.winfo_ismapped() == 0:
+        c1.grid(row=3, column=1, ipadx=2, ipady=2, rowspan=1, columnspan=2)
+    if c2.winfo_ismapped() == 0:
+        c2.grid(row=3, column=2, ipadx=2, ipady=2, rowspan=1, columnspan=2)
     if l3.winfo_ismapped() == 0:
         l3.grid(row=2, column=1, sticky=tkinter.W, padx=10, pady=10, ipadx=2, ipady=2)
     if b5.winfo_ismapped() == 0:
@@ -527,6 +750,52 @@ def download():
     status_var.set("Welcome to test case downloader!")
     change_color(0)
 
+
+def multisource():
+    FileNameStr.set("")
+    Clientstr.set("")
+    CheckVar1.set(0)
+    CheckVar3.set(1)
+    CheckVar4.set(1)
+    CheckVar5.set(1)
+    b1.grid_remove()
+    b2.grid_remove()
+    b3.grid_remove()
+    b4.grid_remove()
+    b5.grid_remove()
+    c1.grid_remove()
+    c2.grid_remove()
+    l3.grid_remove()
+    l5.grid_remove()
+    e1.grid_remove()
+    if e2["state"] == "disabled":
+        e2["state"] = "normal"
+    if l4.winfo_ismapped() == 0:
+        l4.grid(row=2, column=1, sticky=tkinter.W, padx=10, pady=10, ipadx=2, ipady=2)
+    if e2.winfo_ismapped() == 0:
+        e2.grid(row=2, column=2, padx=10, pady=10, ipadx=2, ipady=2)
+    if d1.winfo_ismapped() == 0:
+        d1.grid(row=1, column=2, padx=10, pady=10, ipadx=2, ipady=2)
+    if b6.winfo_ismapped() == 0:
+        b6.grid(row=4, column=1, padx=10, pady=10, rowspan=3, columnspan=3)
+    if c3.winfo_ismapped() == 0:
+        c3.grid(row=3, column=1, ipadx=2, ipady=2, sticky=tkinter.EW)
+    if c4.winfo_ismapped() == 0:
+        c4.grid(row=3, column=2, ipadx=2, ipady=2, sticky=tkinter.EW)
+    if c5.winfo_ismapped() == 0:
+        c5.grid(row=3, column=3, ipadx=2, ipady=2, sticky=tkinter.EW)
+
+    status_var.set("Welcome to test case downloader!")
+    change_color(0)
+
+
+# Multisource Variables
+CheckVar3 = tkinter.IntVar()
+CheckVar4 = tkinter.IntVar()
+CheckVar5 = tkinter.IntVar()
+c3 = tkinter.Checkbutton(window, text="ArielPayroll", variable=CheckVar3, font=font3)
+c4 = tkinter.Checkbutton(window, text="Ariel360", variable=CheckVar4, font=font3)
+c5 = tkinter.Checkbutton(window, text="ArielDB", variable=CheckVar5, font=font3)
 
 # Status Bar
 status_var = tkinter.StringVar()
@@ -549,9 +818,10 @@ img = img.subsample(4,4)
 logo = tkinter.Label(master=window, image=img, background='white')
 # Labels and Entry
 l1 = tkinter.Label(window, text=brand, background='white')
-l2 = tkinter.Label(window, text="CLIENT NAME:", font=font1, borderwidth=1, relief="solid")
-l3 = tkinter.Label(window, text="TEST FILE NAME:", font=font1, borderwidth=1, relief="solid")
-l5 = tkinter.Label(window, text="ENVELOPE HEADER:", font=font1, borderwidth=1, relief="solid")
+l2 = tkinter.Label(window, text="CLIENT NAME :", font=font1, borderwidth=0, relief="solid")
+l3 = tkinter.Label(window, text="TEST CASE NAME :", font=font1, borderwidth=0, relief="solid")
+l4 = tkinter.Label(window, text="EMPLOYEE ID :", font=font1, borderwidth=0, relief="solid")
+l5 = tkinter.Label(window, text="ENVELOPE HEADER :", font=font1, borderwidth=0, relief="solid")
 e1 = tkinter.Entry(window, width=31, font=font4, borderwidth=1, relief="solid",textvariable=addclientstr, selectbackground="Yellow", selectforeground="black")
 e2 = tkinter.Entry(window, width=31, font=font4, borderwidth=1, relief="solid",textvariable=FileNameStr, selectbackground="Yellow", selectforeground="black")
 
@@ -563,6 +833,7 @@ refreshClients()
 # Radio Buttons and Action Buttons
 c1 = tkinter.Radiobutton(window, text="Ariel360", variable=CheckVar1, value=1, font=font3)
 c2 = tkinter.Radiobutton(window, text="ArielDB", variable=CheckVar1, value=2, font=font3)
+
 b1 = tkinter.Button(window, text="ENVELOPE", command=submitFunction, font=font1)
 b1.config(width=15, height=1)
 b2 = tkinter.Button(window, text="AUTOMATE", command=AutomateFunction, font=font1)
@@ -573,9 +844,11 @@ b4 = tkinter.Button(window, text="DELETE", command=delFunction, font=font1)
 b4.config(width=15, height=1)
 b5 = tkinter.Button(window, text="DOWNLOAD", command=downloadFunction, font=font1)
 b5.config(width=15, height=1)
+b6 = tkinter.Button(window, text="MULTISOURCE", command=multisourceFunction, font=font1)
+b6.config(width=15, height=1)
 
 # Menubar Options Help
-font5 = ('Times',12,'bold')
+font5 = ('Times', 12, 'bold')
 menubar = tkinter.Menu()
 window.config(menu=menubar)
 options_menu = tkinter.Menu(menubar)
@@ -590,6 +863,7 @@ options_menu.add_command(label="Automate",font=font5, command=lambda: auto_mate(
 options_menu.add_command(label="Download",font=font5, command=lambda: download())
 options_menu.add_command(label="Add Client",font=font5, command=lambda:addClient())
 options_menu.add_command(label="Delete Client",font=font5, command=lambda:delClient())
+options_menu.add_command(label="MultisourceXML",font=font5, command=lambda:multisource())
 options_menu.add_command(label="Exit",font=font5, command=lambda:quit_app())
 help_menu.add_command(label="About",font=font5, command=lambda:show_about_info())
 
